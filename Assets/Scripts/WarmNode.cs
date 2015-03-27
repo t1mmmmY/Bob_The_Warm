8d13e60f135575ea57a29b90ea3fcb00df962584
denis.dovganich <denis.dovganich@timecode.co>
2015-03-27 18:27:35 +0200

    Warm picker

---
+++ Assets/Scripts/WarmNode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarmNode : MonoBehaviour 
+{
+	private Rigidbody rigidbody;
+
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	public void TakeNode()
+	{
+		rigidbody.isKinematic = true;
+	}
+
+	public void ReleaseNode()
+	{
+		rigidbody.isKinematic = false;
+	}
+
+	public void MoveTo(Vector3 position)
+	{
+		Vector3 newPos = new Vector3(position.x, position.y, transform.position.z);
+		transform.position = newPos;
+	}
+
+}
---
+++ Assets/Scripts/WarmPicker.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarmPicker : MonoBehaviour 
+{
+	Camera mainCamera;
+	WarmNode pickUpNode;
+
+	void Start()
+	{
+		mainCamera = Camera.main;
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			TryToPickUp();
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			TryToReleaseNode();
+		}
+
+		if (pickUpNode != null)
+		{
+			MoveWarmNode();
+		}
+	}
+
+	void TryToPickUp()
+	{
+		Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+//		LayerMask mask = new LayerMask();
+//		mask.value = LayerMask.NameToLayer("Warm");
+
+		if (Physics.Raycast(ray, out hit, 100, 1 << 8))
+		{
+			pickUpNode = hit.collider.GetComponent<WarmNode>();
+			if (pickUpNode != null)
+			{
+				pickUpNode.TakeNode();
+			}
+		}
+	}
+
+	void TryToReleaseNode()
+	{
+		if (pickUpNode != null)
+		{
+			pickUpNode.ReleaseNode();
+			pickUpNode = null;
+		}
+	}
+
+	void MoveWarmNode()
+	{
+		Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+//		LayerMask mask = new LayerMask();
+//		mask.value = LayerMask.NameToLayer("Default");
+
+		if (Physics.Raycast(ray, out hit, 100, ~(1 << 8)))
+		{
+			pickUpNode.MoveTo(hit.point);
+		}
+	}
+
+}
